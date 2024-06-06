2908f36539a6a55b87c65f0b1131e1159b1628a1
Kiefciman <kiefciman@gmail.com>
2024-06-06 18:15:38 +0300

    finished new design

---
+++ index.js
@@ -0,0 +1,44 @@
+function OpenPage(page) {
+   window.location.replace(page + '.html')
+}
+
+function OpenWin(win) {
+    switch(win) {
+        case 'github':
+            window.open('https://github.com/Kiefciman')
+            break
+        case 'reddit':
+            window.open('https://www.reddit.com/user/Kiefciman/')
+            break
+        case 'discord':
+            window.open('https://discordapp.com/users/535905819196260363')
+            break
+        case 'steam':
+            window.open('https://steamcommunity.com/profiles/76561198884191391')
+            break
+        case 'itch.io':
+            window.open('https://kiefciman.itch.io/')
+            break
+        case 'nekoboard':
+            window.open('https://github.com/Kiefciman/Nekoboard')
+            break
+        case 'myanimestats':
+            window.open('https://github.com/Kiefciman/myanimestats')
+            break
+        case 'mymangastats':
+            window.open('https://github.com/Kiefciman/mymangastats')
+            break
+        case 'dwm-kiefciman':
+            window.open('https://github.com/Kiefciman/dwm-kiefciman')
+            break
+        case 'nekofetch':
+            window.open('https://github.com/Kiefciman/nekofetch')
+            break
+        case 'keqinging':
+            window.open('https://github.com/Kiefciman/keqinging')
+            break
+        case 'nekofying':
+            window.open('https://kiefciman.github.io/Nekofying/')
+            break
+    }
+}
