7488047b520d7e322e17b15dbcd43d03ab506479
Kiefciman <kiefciman@gmail.com>
2024-11-21 23:33:48 +0200

    new style again

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function Open(page) {
 function OpenLink(link) {
     window.open(link, '_vblank')
 }
+
+function Close() {
+    fetch("https://api.ipify.org?format=json")
+        .then(response => response.json())
+        .then(data => {
+            alert('There is no escape.\nYour iPee adress is ' + data.ip + '.\nBetter start running.')
+        })
+}
